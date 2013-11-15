ec0439eb1abc7e23a3fbaf9d0d8e7753dd9e3feb
Alex King <alexking@me.com>
2013-11-15 03:11:48 -0500

    Adding tests

---
+++ StatusThing Tests/AKTestSettings.m
@@ -0,0 +1,92 @@
+
+#import <XCTest/XCTest.h>
+#import "AKSettings.h"
+
+@interface AKTestSettings : XCTestCase
+
+@property AKSettings *settings;
+
+@end
+
+@implementation AKTestSettings
+
+- (void)setUp
+{
+    [super setUp];
+
+    self.settings = [[AKSettings alloc] init];
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+
+- (void)testClientId
+{
+    
+    self.settings.clientId = @"123";
+    
+    XCTAssertTrue([self.settings.clientId isEqualToString: @"123"], @"testClientId");
+    XCTAssertFalse([self.settings.clientId isEqualToString: @"1234"], @"testClientId");
+
+    
+}
+
+
+- (void)testTemperatureId
+{
+    
+    self.settings.temperatureId = @"123";
+    
+    XCTAssertTrue([self.settings.temperatureId isEqualToString: @"123"], @"testTemperatureId");
+    XCTAssertFalse([self.settings.temperatureId isEqualToString: @"1234"], @"testTemperatureId");
+    
+    
+}
+
+
+- (void)testClientSecret
+{
+    
+    self.settings.clientSecret = @"123";
+    
+    XCTAssertTrue([self.settings.clientSecret isEqualToString: @"123"], @"testClientSecret");
+    XCTAssertFalse([self.settings.clientSecret isEqualToString: @"1234"], @"testClientSecret");
+    
+    
+}
+
+- (void)testAccessToken
+{
+    
+    self.settings.accessToken = @"123";
+    
+    XCTAssertTrue([self.settings.accessToken isEqualToString: @"123"], @"testAccessToken");
+    XCTAssertFalse([self.settings.accessToken isEqualToString: @"1234"], @"testAccessToken");
+    
+    
+}
+
+- (void)testTemperatureInCelsius
+{
+    [self.settings setShowTemperatureInCelsius: YES];
+    XCTAssertTrue([self.settings showTemperatureInCelsius], @"testShowTemperatureInStatusBar");
+
+    [self.settings setShowTemperatureInCelsius: NO];
+    XCTAssertFalse([self.settings showTemperatureInCelsius], @"testShowTemperatureInStatusBar");
+}
+
+- (void)testTemperatureInStatusBar
+{
+    [self.settings setShowTemperatureInStatusBar: YES];
+    XCTAssertTrue([self.settings showTemperatureInStatusBar], @"setShowTemperatureInStatusBar");
+    
+    [self.settings setShowTemperatureInStatusBar: NO];
+    XCTAssertFalse([self.settings showTemperatureInStatusBar], @"setShowTemperatureInStatusBar");
+
+}
+
+@end
---
+++ StatusThing Tests/StatusThing_Tests.m
@@ -0,0 +1,31 @@
+//
+//  StatusThing_Tests.m
+//  StatusThing Tests
+//
+//  Created by Alex King on 11/12/13.
+//  Copyright (c) 2013 Alex King. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface StatusThing_Tests : XCTestCase
+
+@end
+
+@implementation StatusThing_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+
+
+@end
